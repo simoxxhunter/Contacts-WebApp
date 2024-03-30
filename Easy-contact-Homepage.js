634aba003a308a70b172182ec1c0147c1255c140
simoxxhunter <94250469+simoxxhunter@users.noreply.github.com>
2024-03-30 20:50:29 +0000

    imprted api outpout to use locally

--- Easy-contact-Homepage.js
+++ Easy-contact-Homepage.js
@@ -1,19 +1,322 @@
-fetch('https://randomuser.me/api/?results=10')
-    .then(response => response.json())
-    .then(data => {
-        data.results.forEach(user => {
-            const fullName = `${user.name.title} ${user.name.first} ${user.name.last}`;
-            const proficiency = `${user.location.city}, ${user.location.country}`;
 
-            const userDiv = document.createElement('div');
-            userDiv.classList.add('main-contacts');
-            userDiv.innerHTML =
-                `<img class="profile-pic rounded-circle" src="${user.picture.medium}" alt="user picture" style="height: 50px; width: 50px;">
-          <div class="name-pro d-flex flex-column align-items-center justify-content-center">
-              <p class="Full-Name">${fullName}</p>
-              <p class="text proficiency">${proficiency}</p>
-          </div> `;
-            document.getElementById('main-contacts').appendChild(userDiv);
-        });
-    })
-    .catch(error => console.error('Error fetching data:', error));
+
+const results = {
+    "results": [{
+        "gender": "male",
+        "name": {
+            "title": "Mr",
+            "first": "Samuel",
+            "last": "Brown"
+        },
+        "location": {
+            "street": {
+                "number": 979,
+                "name": "Western Hills Drive"
+            },
+            "city": "Lower Hutt",
+            "state": "West Coast",
+            "country": "New Zealand",
+            "postcode": 94997,
+            "coordinates": {
+                "latitude": "50.1666",
+                "longitude": "-68.5079"
+            },
+            "timezone": {
+                "offset": "+4:30",
+                "description": "Kabul"
+            }
+        },
+        "email": "samuel.brown@example.com",
+        "login": {
+            "uuid": "e0a1f6ff-13ab-4340-9ec9-daa5a7676902",
+            "username": "silvermeercat882",
+            "password": "tinman",
+            "salt": "foQUpqbJ",
+            "md5": "773e0462e80ce1ee850ac4941e96f7ac",
+            "sha1": "974f39758761236cf166cc311cbccad06692fc99",
+            "sha256": "3e4dd646afdde2fe0736411a374ef3287bfc400bebf8722c3cbd5e4603995e23"
+        },
+        "dob": {
+            "date": "1981-04-26T21:36:01.403Z",
+            "age": 42
+        },
+        "registered": {
+            "date": "2014-12-16T11:03:28.017Z",
+            "age": 9
+        },
+        "phone": "(643)-446-0190",
+        "cell": "(721)-981-0322",
+        "id": {
+            "name": "",
+            "value": null
+        },
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/33.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/33.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/33.jpg"
+        },
+        "nat": "NZ"
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "Mr",
+            "first": "Samuel",
+            "last": "Brown"
+        },
+        "location": {
+            "street": {
+                "number": 979,
+                "name": "Western Hills Drive"
+            },
+            "city": "Lower Hutt",
+            "state": "West Coast",
+            "country": "New Zealand",
+            "postcode": 94997,
+            "coordinates": {
+                "latitude": "50.1666",
+                "longitude": "-68.5079"
+            },
+            "timezone": {
+                "offset": "+4:30",
+                "description": "Kabul"
+            }
+        },
+        "email": "samuel.brown@example.com",
+        "login": {
+            "uuid": "e0a1f6ff-13ab-4340-9ec9-daa5a7676902",
+            "username": "silvermeercat882",
+            "password": "tinman",
+            "salt": "foQUpqbJ",
+            "md5": "773e0462e80ce1ee850ac4941e96f7ac",
+            "sha1": "974f39758761236cf166cc311cbccad06692fc99",
+            "sha256": "3e4dd646afdde2fe0736411a374ef3287bfc400bebf8722c3cbd5e4603995e23"
+        },
+        "dob": {
+            "date": "1981-04-26T21:36:01.403Z",
+            "age": 42
+        },
+        "registered": {
+            "date": "2014-12-16T11:03:28.017Z",
+            "age": 9
+        },
+        "phone": "(643)-446-0190",
+        "cell": "(721)-981-0322",
+        "id": {
+            "name": "",
+            "value": null
+        },
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/33.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/33.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/33.jpg"
+        },
+        "nat": "NZ"
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "Mr",
+            "first": "Samuel",
+            "last": "Brown"
+        },
+        "location": {
+            "street": {
+                "number": 979,
+                "name": "Western Hills Drive"
+            },
+            "city": "Lower Hutt",
+            "state": "West Coast",
+            "country": "New Zealand",
+            "postcode": 94997,
+            "coordinates": {
+                "latitude": "50.1666",
+                "longitude": "-68.5079"
+            },
+            "timezone": {
+                "offset": "+4:30",
+                "description": "Kabul"
+            }
+        },
+        "email": "samuel.brown@example.com",
+        "login": {
+            "uuid": "e0a1f6ff-13ab-4340-9ec9-daa5a7676902",
+            "username": "silvermeercat882",
+            "password": "tinman",
+            "salt": "foQUpqbJ",
+            "md5": "773e0462e80ce1ee850ac4941e96f7ac",
+            "sha1": "974f39758761236cf166cc311cbccad06692fc99",
+            "sha256": "3e4dd646afdde2fe0736411a374ef3287bfc400bebf8722c3cbd5e4603995e23"
+        },
+        "dob": {
+            "date": "1981-04-26T21:36:01.403Z",
+            "age": 42
+        },
+        "registered": {
+            "date": "2014-12-16T11:03:28.017Z",
+            "age": 9
+        },
+        "phone": "(643)-446-0190",
+        "cell": "(721)-981-0322",
+        "id": {
+            "name": "",
+            "value": null
+        },
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/33.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/33.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/33.jpg"
+        },
+        "nat": "NZ"
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "Mr",
+            "first": "Samuel",
+            "last": "Brown"
+        },
+        "location": {
+            "street": {
+                "number": 979,
+                "name": "Western Hills Drive"
+            },
+            "city": "Lower Hutt",
+            "state": "West Coast",
+            "country": "New Zealand",
+            "postcode": 94997,
+            "coordinates": {
+                "latitude": "50.1666",
+                "longitude": "-68.5079"
+            },
+            "timezone": {
+                "offset": "+4:30",
+                "description": "Kabul"
+            }
+        },
+        "email": "samuel.brown@example.com",
+        "login": {
+            "uuid": "e0a1f6ff-13ab-4340-9ec9-daa5a7676902",
+            "username": "silvermeercat882",
+            "password": "tinman",
+            "salt": "foQUpqbJ",
+            "md5": "773e0462e80ce1ee850ac4941e96f7ac",
+            "sha1": "974f39758761236cf166cc311cbccad06692fc99",
+            "sha256": "3e4dd646afdde2fe0736411a374ef3287bfc400bebf8722c3cbd5e4603995e23"
+        },
+        "dob": {
+            "date": "1981-04-26T21:36:01.403Z",
+            "age": 42
+        },
+        "registered": {
+            "date": "2014-12-16T11:03:28.017Z",
+            "age": 9
+        },
+        "phone": "(643)-446-0190",
+        "cell": "(721)-981-0322",
+        "id": {
+            "name": "",
+            "value": null
+        },
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/33.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/33.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/33.jpg"
+        },
+        "nat": "NZ"
+    },
+    {
+        "gender": "male",
+        "name": {
+            "title": "Mr",
+            "first": "Samuel",
+            "last": "Brown"
+        },
+        "location": {
+            "street": {
+                "number": 979,
+                "name": "Western Hills Drive"
+            },
+            "city": "Lower Hutt",
+            "state": "West Coast",
+            "country": "New Zealand",
+            "postcode": 94997,
+            "coordinates": {
+                "latitude": "50.1666",
+                "longitude": "-68.5079"
+            },
+            "timezone": {
+                "offset": "+4:30",
+                "description": "Kabul"
+            }
+        },
+        "email": "samuel.brown@example.com",
+        "login": {
+            "uuid": "e0a1f6ff-13ab-4340-9ec9-daa5a7676902",
+            "username": "silvermeercat882",
+            "password": "tinman",
+            "salt": "foQUpqbJ",
+            "md5": "773e0462e80ce1ee850ac4941e96f7ac",
+            "sha1": "974f39758761236cf166cc311cbccad06692fc99",
+            "sha256": "3e4dd646afdde2fe0736411a374ef3287bfc400bebf8722c3cbd5e4603995e23"
+        },
+        "dob": {
+            "date": "1981-04-26T21:36:01.403Z",
+            "age": 42
+        },
+        "registered": {
+            "date": "2014-12-16T11:03:28.017Z",
+            "age": 9
+        },
+        "phone": "(643)-446-0190",
+        "cell": "(721)-981-0322",
+        "id": {
+            "name": "",
+            "value": null
+        },
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/33.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/33.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/33.jpg"
+        },
+        "nat": "NZ"
+    },
+    ],
+    
+};
+
+results.results.forEach(user => {
+    const fullName = `${user.name.title} ${user.name.first} ${user.name.last}`;
+    const proficiency = `${user.location.city}, ${user.location.country}`;
+
+    const userDiv = document.createElement('div');
+    userDiv.classList.add('main-contacts');
+    userDiv.innerHTML =
+        `<img class="profile-pic rounded-circle" src="${user.picture.medium}" alt="user picture" style="height: 50px; width: 50px;">
+  <div class="name-pro d-flex flex-column align-items-center justify-content-center">
+      <p class="Full-Name h2">${fullName}</p>
+      <p class="text proficiency">${proficiency}</p>
+  </div> `;
+    document.getElementById('main-contacts').appendChild(userDiv);
+});
+
+
+// fetch('https://randomuser.me/api/?results=10')
+//     .then(response => response.json())
+//     .then(data => {
+//         data.results.forEach(user => {
+//             const fullName = `${user.name.title} ${user.name.first} ${user.name.last}`;
+//             const proficiency = `${user.location.city}, ${user.location.country}`;
+
+//             const userDiv = document.createElement('div');
+//             userDiv.classList.add('main-contacts');
+//             userDiv.innerHTML =
+//                 `<img class="profile-pic rounded-circle" src="${user.picture.medium}" alt="user picture" style="height: 50px; width: 50px;">
+//           <div class="name-pro d-flex flex-column align-items-center justify-content-center">
+//               <p class="Full-Name h2">${fullName}</p>
+//               <p class="text proficiency">${proficiency}</p>
+//           </div> `;
+//             document.getElementById('main-contacts').appendChild(userDiv);
+//         });
+//     })
+//     .catch(error => console.error('Error fetching data:', error));
